252a716afd32d194ae67bd2bc754d5af992413d8
seanclaybaugh <sdclaybaugh@gmail.com>
2021-08-26 11:43:34 -0700

    created about component

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>All About this page</h2>
+    <p>
+      Thinking about hiring me? Cool!
+    </p>
+  </div>
+);
+
+export default AboutPage;
