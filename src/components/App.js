6aac78ec73873af5ddc224c6bdf092c6349911e8
seanclaybaugh <sdclaybaugh@gmail.com>
2021-09-10 10:44:34 -0700

    added 404 route

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
 import Header from './common/Header';
+import PageNotFound from './PageNotFound'
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+Switch will make the program stop looking for other routes once one route matches.
+
+*/
