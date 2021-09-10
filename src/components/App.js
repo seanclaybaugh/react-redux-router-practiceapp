00255f92313f900fb17a5d838622a5e0b2e61432
seanclaybaugh <sdclaybaugh@gmail.com>
2021-09-10 16:35:20 -0700

    added redux concept notes

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,43 @@ export default App;
 
 /*
 
-Switch will make the program stop looking for other routes once one route matches.
+Switch - will make the program stop looking for other routes once one route matches.
 Here, because the last component is the page not found, it will go through each route until it hits the last undefined route
+
+3 Main Principles of Redux:
+ - one immutable store
+ - actions trigger changes
+ - reducers update state, pure functions that accept current state and an action and returns a new state
+
+ ***Check "A Chat with Redux" under Connecting React to Redux for a high level download on communication flow between React/Actions/Reducers/Store
+
+ ------------------------
+ ACTION CREATOR EX:
+
+    rateCourse(rating) {
+      return { type: RATE_COURSE, rating: rating }
+    }
+
+    'type' is required
+-------------------------
+
+store.dispatch(action)
+store.subscribe(listener)
+store.getState()
+replaceReducer(nextReducer)
+
+------------------------------------
+REDUCERS:
+
+function myReducer(state, action) {
+  //return new state based on action
+}
+
+Never:
+Mutate args
+Perform side effects
+Call non-pure functions (return should always be the same with the same args)
+
+
+
 */
