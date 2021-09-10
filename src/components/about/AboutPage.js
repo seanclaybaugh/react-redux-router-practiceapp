17fe67f81c9feccc4b3997da1baee5973dddad29
seanclaybaugh <sdclaybaugh@gmail.com>
2021-09-10 10:26:56 -0700

    added navbar

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -4,7 +4,7 @@ const AboutPage = () => (
   <div>
     <h2>All About this page</h2>
     <p>
-      Thinking about hiring me? Cool!
+      Thinking about hiring me? Cool! This app uses React, Redux, React Router and Jest/React testing library
     </p>
   </div>
 );
